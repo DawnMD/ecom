6a999b5eeb3d5c2a96636b64669089f8d687f3aa
Mainak Das <mainakdas104@gmail.com>
2026-03-03 02:45:17 +0530

    Enhance homepage layout with product filtering and sorting components
    - Updated the `Page` component to include `ProductFilter`, `PopularitySortDropdown`, and `ViewModeToggle` for improved product browsing experience.
    - Added a responsive grid layout to display products and sorting options effectively.
    - Introduced a separator for better visual distinction between components.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,29 @@
 import { Banner } from "@/components/banner";
+import { PopularitySortDropdown } from "@/components/popularity-sort-dropdown";
+import { ProductFilter } from "@/components/product-filter";
+import { ViewModeToggle } from "@/components/view-mode-toggle";
+import { Separator } from "@/components/ui/separator";
 
 export default function Page() {
   return (
     <>
       <Banner />
-      <div>home page</div>
+      <div className="p-4 lg:p-8 flex flex-col gap-4 lg:gap-8">
+        <p className="text-2xl font-bold">6 products found</p>
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-4">
+          <div className="hidden lg:block lg:col-span-2">
+            <ProductFilter />
+          </div>
+          <div className="lg:col-span-10 flex flex-col gap-4">
+            <div className="flex items-center justify-between gap-3">
+              <ViewModeToggle />
+              <PopularitySortDropdown />
+            </div>
+            <Separator />
+            <div>gird</div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
